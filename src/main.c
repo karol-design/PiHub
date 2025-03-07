69935aa296bd4ae13d1f6586045934264b0ae961
Karol Wojslaw <karol.wojslaw@globallogic.com>
2025-03-07 14:49:22 +0100

    Update test_piHub and main with new components architecture

--- src/main.c
+++ src/main.c
@@ -1,18 +1,20 @@
 #include <string.h> // For: strlen()
 #include <unistd.h> // For: sleep()
 
-#include "app/parser.h"
+#include "app/dispatcher.h"
 #include "comm/network.h"
+
+#define LOGS_ENABLED 1
 #include "utils/log.h"
 
 void test_server();
-void test_parser();
+void test_dispatcher();
 void test_ll();
 
 int main() {
-    test_server();
-    // test_parser();
+    // test_dispatcher();
     // test_ll();
+    test_server();
     return 0;
 }
 
@@ -24,20 +26,20 @@ void handle_client_connect(void* ctx, const ServerClient_t client) {
     Server_t* _ctx = (Server_t*)ctx;
 
     char ip_str[IPV4_ADDRSTR_LENGHT];
-    ServerError_t err = _ctx->get_client_ip(client, ip_str);
-    log_debug("server->get_client_ip called (ret: %d)", err);
+    ServerError_t err = server_get_client_ip(client, ip_str);
+    log_debug("server_get_client_ip called (ret: %d)", err);
 
     char msg_client[64] = "Welcome ";
     strlcat(msg_client, ip_str, 64);
     strlcat(msg_client, "!\n", 64);
-    err = _ctx->write(_ctx, client, msg_client, strlen(msg_client));
-    log_debug("server->write called (ret: %d)", err);
+    err = server_write(_ctx, client, msg_client, strlen(msg_client));
+    log_debug("server_write called (ret: %d)", err);
 
     char msg_broadcast[64] = "New client (";
     strlcat(msg_broadcast, ip_str, 64);
     strlcat(msg_broadcast, ") joined the session!\n", 64);
-    err = _ctx->broadcast(_ctx, msg_broadcast, strlen(msg_broadcast));
-    log_debug("server->broadcast called (ret: %d)", err);
+    err = server_broadcast(_ctx, msg_broadcast, strlen(msg_broadcast));
+    log_debug("server_broadcast called (ret: %d)", err);
 }
 
 void handle_data_received(void* ctx, const ServerClient_t client) {
@@ -46,8 +48,8 @@ void handle_data_received(void* ctx, const ServerClient_t client) {
     Server_t* _ctx = (Server_t*)ctx;
     char buf[100];
     ssize_t len;
-    ServerError_t err = _ctx->read(ctx, client, buf, 100, &len);
-    log_debug("server->read called (ret: %d)", err);
+    ServerError_t err = server_read(ctx, client, buf, 100, &len);
+    log_debug("server_read called (ret: %d)", err);
 }
 
 void handle_client_disconnect(void* ctx, const ServerClient_t client) {
@@ -56,8 +58,8 @@ void handle_client_disconnect(void* ctx, const ServerClient_t client) {
     Server_t* _ctx = (Server_t*)ctx;
 
     const char* msg_broadcast = "One of the clients disconnected from the session!\n";
-    ServerError_t err = _ctx->broadcast(_ctx, msg_broadcast, strlen(msg_broadcast));
-    log_debug("server->broadcast called (ret: %d)", err);
+    ServerError_t err = server_broadcast(_ctx, msg_broadcast, strlen(msg_broadcast));
+    log_debug("server_broadcast called (ret: %d)", err);
 }
 
 void handle_server_failure(void* ctx, const ServerError_t err) {
@@ -65,22 +67,22 @@ void handle_server_failure(void* ctx, const ServerError_t err) {
     log_info("handle_server_failure called with error: %d", err);
     log_info("attempting to restart the server");
 
-    // Shut down the server...
-    ServerError_t ret = _ctx->shutdown(_ctx);
-    log_debug("server->shutdown called (ret: %d)", err);
+    // Shut down the server_..
+    ServerError_t ret = server_shutdown(_ctx);
+    log_debug("server_shutdown called (ret: %d)", err);
 
     // ...and restart it
-    ret = _ctx->run(_ctx);
-    log_debug("server->run called (ret: %d)", err);
+    ret = server_run(_ctx);
+    log_debug("server_run called (ret: %d)", err);
 }
 
-/************* Event handlers for Parser *************/
+/************* Event handlers for Dispatcher *************/
 void handle_gpio_set(char* argv, uint32_t argc) {
     log_info("handle_gpio_set called");
 
-    for(uint32_t arg = 0; (arg < argc) && (arg < PARSER_MAX_ARGS); arg++) {
-        if(strnlen(argv + (arg * PARSER_ARG_MAX_SIZE), PARSER_ARG_MAX_SIZE) < PARSER_ARG_MAX_SIZE) {
-            log_info("  arg %d: %s", arg, argv + (arg * PARSER_ARG_MAX_SIZE));
+    for(uint32_t arg = 0; (arg < argc) && (arg < DISPATCHER_MAX_ARGS); arg++) {
+        if(strnlen(argv + (arg * DISPATCHER_ARG_MAX_SIZE), DISPATCHER_ARG_MAX_SIZE) < DISPATCHER_ARG_MAX_SIZE) {
+            log_info("  arg %d: %s", arg, argv + (arg * DISPATCHER_ARG_MAX_SIZE));
         }
     }
 }
@@ -88,9 +90,9 @@ void handle_gpio_set(char* argv, uint32_t argc) {
 void handle_gpio_toggle(char* argv, uint32_t argc) {
     log_info("handle_gpio_toggle called");
 
-    for(uint32_t arg = 0; (arg < argc) && (arg < PARSER_MAX_ARGS); arg++) {
-        if(strnlen(argv + (arg * PARSER_ARG_MAX_SIZE), PARSER_ARG_MAX_SIZE) < PARSER_ARG_MAX_SIZE) {
-            log_info("  arg %d: %s", arg, argv + (arg * PARSER_ARG_MAX_SIZE));
+    for(uint32_t arg = 0; (arg < argc) && (arg < DISPATCHER_MAX_ARGS); arg++) {
+        if(strnlen(argv + (arg * DISPATCHER_ARG_MAX_SIZE), DISPATCHER_ARG_MAX_SIZE) < DISPATCHER_ARG_MAX_SIZE) {
+            log_info("  arg %d: %s", arg, argv + (arg * DISPATCHER_ARG_MAX_SIZE));
         }
     }
 }
@@ -98,9 +100,9 @@ void handle_gpio_toggle(char* argv, uint32_t argc) {
 void handle_sensor_list(char* argv, uint32_t argc) {
     log_info("handle_sensor_list called");
 
-    for(uint32_t arg = 0; (arg < argc) && (arg < PARSER_MAX_ARGS); arg++) {
-        if(strnlen(argv + (arg * PARSER_ARG_MAX_SIZE), PARSER_ARG_MAX_SIZE) < PARSER_ARG_MAX_SIZE) {
-            log_info("  arg %d: %s", arg, argv + (arg * PARSER_ARG_MAX_SIZE));
+    for(uint32_t arg = 0; (arg < argc) && (arg < DISPATCHER_MAX_ARGS); arg++) {
+        if(strnlen(argv + (arg * DISPATCHER_ARG_MAX_SIZE), DISPATCHER_ARG_MAX_SIZE) < DISPATCHER_ARG_MAX_SIZE) {
+            log_info("  arg %d: %s", arg, argv + (arg * DISPATCHER_ARG_MAX_SIZE));
         }
     }
 }
@@ -118,71 +120,72 @@ ListError_t print_node(void* data) {
 
 /************* Functional tests *************/
 
-void test_parser() {
-    Parser_t parser;
-    ParserConfig_t cfg = { .delim = " " };
-    ParserCommandDef_t gpio_set_cmd = { .target = "gpio", .action = "set", .callback_ptr = handle_gpio_set };
-    ParserCommandDef_t gpio_toggle_cmd = { .target = "gpio", .action = "toggle", .callback_ptr = handle_gpio_toggle };
-    ParserCommandDef_t sensor_list_cmd = { .target = "sensor", .action = "list", .callback_ptr = handle_sensor_list };
+void test_dispatcher() {
+    Dispatcher_t dispatcher;
+    DispatcherConfig_t cfg = { .delim = " " };
+    DispatcherCommandDef_t gpio_set_cmd = { .target = "gpio", .action = "set", .callback_ptr = handle_gpio_set };
+    DispatcherCommandDef_t gpio_toggle_cmd = { .target = "gpio", .action = "toggle", .callback_ptr = handle_gpio_toggle };
+    DispatcherCommandDef_t sensor_list_cmd = { .target = "sensor", .action = "list", .callback_ptr = handle_sensor_list };
 
-    ParserError_t err = parser_init(&parser, cfg);
-    log_debug("parser_init called (ret %d)", err);
+    DispatcherError_t err = dispatcher_init(&dispatcher, cfg);
+    log_info("dispatcher_init called (ret %d)", err);
 
-    err = parser.add_cmd(&parser, 0, gpio_set_cmd);
-    log_debug("parser.add_cmd called (ret %d)", err);
+    err = dispatcher_register(&dispatcher, 0, gpio_set_cmd);
+    log_info("dispatcher_register called (ret %d)", err);
 
     // Test too many args
-    err = parser.execute(&parser, "gpio set 1 2 3 4 5 6 7 8 9 10 11");
-    log_debug("parser.execute called (ret %d)", err);
+    err = dispatcher_execute(&dispatcher, "gpio set 1 2 3 4 5 6 7 8 9 10 11");
+    log_info("dispatcher_execute called (ret %d)", err);
 
     // Test empty buffer
-    err = parser.execute(&parser, " ");
-    log_debug("parser.execute called (ret %d)", err);
+    err = dispatcher_execute(&dispatcher, " ");
+    log_info("dispatcher_execute called (ret %d)", err);
 
     // Test NULL ptr
-    err = parser.execute(&parser, NULL);
-    log_debug("parser.execute called (ret %d)", err);
+    err = dispatcher_execute(&dispatcher, NULL);
+    log_info("dispatcher_execute called (ret %d)", err);
 
     // Test case-sensitivity
-    err = parser.execute(&parser, "GPiO SeT 0 OK");
-    log_debug("parser.execute called (ret %d)", err);
+    err = dispatcher_execute(&dispatcher, "GPiO SeT 0 OK");
+    log_info("dispatcher_execute called (ret %d)", err);
 
     // Test unknown command
-    err = parser.execute(&parser, "GPiO SeTs 0");
-    log_debug("parser.execute called (ret %d)", err);
+    err = dispatcher_execute(&dispatcher, "GPiO SeTs 0");
+    log_info("dispatcher_execute called (ret %d)", err);
 
     /* ------------------ */
 
-    err = parser.add_cmd(&parser, 1, gpio_toggle_cmd);
-    log_debug("parser.add_cmd called (ret %d)", err);
+    err = dispatcher_register(&dispatcher, 1, gpio_toggle_cmd);
+    log_info("dispatcher_register called (ret %d)", err);
 
     // Test case-sensitivity
-    err = parser.execute(&parser, "GPiO toGGle PIN1 OK");
-    log_debug("parser.execute called (ret %d)", err);
+    err = dispatcher_execute(&dispatcher, "GPiO toGGle PIN1 OK");
+    log_info("dispatcher_execute called (ret %d)", err);
 
     /* ------------------ */
 
-    err = parser.add_cmd(&parser, 2, sensor_list_cmd);
-    log_debug("parser.add_cmd called (ret %d)", err);
+    err = dispatcher_register(&dispatcher, 2, sensor_list_cmd);
+    log_info("dispatcher_register called (ret %d)", err);
 
     // Test case-sensitivity
-    err = parser.execute(&parser, "Sensor LIST S1 OK");
-    log_debug("parser.execute called (ret %d)", err);
+    err = dispatcher_execute(&dispatcher, "Sensor LIST S1 OK");
+    log_info("dispatcher_execute called (ret %d)", err);
 
     // Test case-sensitivity
-    err = parser.execute(&parser, "GPiO toGGle PIN1 OK");
-    log_debug("parser.execute called (ret %d)", err);
+    err = dispatcher_execute(&dispatcher, "GPiO toGGle PIN1 OK");
+    log_info("dispatcher_execute called (ret %d)", err);
 
     /* ------------------ */
 
-    err = parser.remove_cmd(&parser, 1);
-    log_debug("parser.remove_cmd called (ret %d)", err);
+    err = dispatcher_deregister(&dispatcher, 1);
+    log_info("dispatcher_deregister called (ret %d)", err);
 
     // Test case-sensitivity
-    err = parser.execute(&parser, "GPiO toGGle PIN1");
-    log_debug("parser.execute called (ret %d)", err);
+    err = dispatcher_execute(&dispatcher, "GPiO toGGle PIN1");
+    log_info("dispatcher_execute called (ret %d)", err);
 
-    parser.deinit(&parser);
+    err = dispatcher_deinit(&dispatcher);
+    log_info("dispatcher_deinit called (ret %d)", err);
 }
 
 void test_server() {
@@ -198,43 +201,43 @@ void test_server() {
     log_debug("server_init called (ret: %d)", err);
 
     // Test: running a server
-    err = server.run(&server);
-    log_debug("server.run called (ret: %d)", err);
+    err = server_run(&server);
+    log_debug("server_run called (ret: %d)", err);
 
     sleep(40);
 
     // Test: printing all clients and their IP addresses
     log_info("Connected clients:");
-    ListNode_t* node = server.get_clients(&server);
+    ListNode_t* node = server_get_clients(&server);
     while(node) {
         char ip_str[IPV4_ADDRSTR_LENGHT];
         ServerClient_t* client = (ServerClient_t*)(node->data);
-        server.get_client_ip(*client, ip_str);
+        server_get_client_ip(*client, ip_str);
         log_info("Client fd: %d, thread id: %lu, ip: %s", client->fd, client->thread, ip_str);
         node = node->next;
     }
 
-    node = server.get_clients(&server);
+    node = server_get_clients(&server);
     if(node) {
         ServerClient_t* client = (ServerClient_t*)(node->data);
 
         // Test: disconnecting a specific client (in this example: ll head)
         const char* msg_client = "You are disconnected!!\n";
-        err = server.write(&server, *client, msg_client, strlen(msg_client));
-        log_debug("server.write called (ret: %d)", err);
-        err = server.disconnect(&server, *client, false);
-        log_debug("server.disconnect called (ret: %d)", err);
+        err = server_write(&server, *client, msg_client, strlen(msg_client));
+        log_debug("server_write called (ret: %d)", err);
+        err = server_disconnect(&server, *client, false);
+        log_debug("server_disconnect called (ret: %d)", err);
     }
 
     sleep(20);
 
     // Test: shutting down the server
-    err = server.shutdown(&server);
-    log_info("server.shutdown called (ret: %d)", err);
+    err = server_shutdown(&server);
+    log_info("server_shutdown called (ret: %d)", err);
 
     // Test: deinit the server instance
-    err = server.deinit(&server);
-    log_info("server.deinit called (ret: %d)", err);
+    err = server_deinit(&server);
+    log_info("server_deinit called (ret: %d)", err);
 
     sleep(5);
 }
--- tests/test_piHub.c
+++ tests/test_piHub.c
@@ -1,17 +1,21 @@
+#include <setjmp.h>
 #include <stdarg.h>
 #include <stddef.h>
-#include <setjmp.h>
 #include <stdint.h>
+// Cmocka must be included last (!)
 #include <cmocka.h>
 
 extern int run_network_tests(void);
+extern int run_llist_tests(void);
+extern int run_dispatcher_tests(void);
 
-int main()
-{
-  // Configure the CMocka results generation
-  cmocka_set_message_output(CM_OUTPUT_STANDARD); // Can be ORed with CM_OUTPUT_XML for .xml generation
+int main() {
+    // Configure the CMocka results generation
+    cmocka_set_message_output(CM_OUTPUT_STANDARD); // Can be ORed with CM_OUTPUT_XML for .xml generation
 
-  int result = 0;
-  result += run_network_tests();
-  return result;
+    int result = 0;
+    // result += run_network_tests();
+    result += run_llist_tests();
+    result += run_dispatcher_tests();
+    return result;
 }
