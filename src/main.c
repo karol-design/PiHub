d65d148acbfe6480d3773c68b674b9398ada7515
Karol Wojslaw <karol.wojslaw@globallogic.com>
2025-05-29 19:16:48 +0200

    add support for running pihub as a systemd daemon

--- src/main.c
+++ src/main.c
@@ -1,12 +1,29 @@
+#include <signal.h>            // for: sig_atomic_t
+#include <systemd/sd-daemon.h> // for: systemd notifications
+
 #include "app/app.h"
 #include "utils/log.h"
 
+volatile sig_atomic_t sig_status = 0;
+
+static void catch_function(int signo) {
+    sig_status = signo;
+}
+
 int main() {
+    setvbuf(stdout, NULL, _IONBF, 0); // Disable stdout buffering completely
+
+    // Set the signal handler for SIGINT
+    if(signal(SIGINT, catch_function) == SIG_ERR) {
+        log_error("failed to set the SIGINT signal handler");
+        return EXIT_FAILURE;
+    }
+
     // Initialize app controller
     AppError_t err = app_init();
     if(err != APP_ERR_OK) {
         log_error("app_init failed (ret: %d)", err);
-        return -1;
+        return EXIT_FAILURE;
     }
     log_info("App controller initialized");
 
@@ -14,16 +31,37 @@ int main() {
     err = app_run();
     if(err != APP_ERR_OK) {
         log_error("app_run failed (ret: %d)", err);
-        return -1;
+        return EXIT_FAILURE;
     }
     log_info("App controller running...");
 
-    // err = app_stop();
-    // err = app_deinit();
-
+    // Notify systemd that the service is ready
+    sd_notify(0, "READY=1");
 
     while(1) {
         // Enter an infinite empty loop once the app controller and the server is started
+        sleep(1);
+
+        // Regularly check for SIGINT and SIGTERM signals to shut down the daemon and exit cleanly
+        if(sig_status == SIGINT || sig_status == SIGTERM) {
+            sd_notify(0, "STOPPING=1"); // Notify systemd that the service will be stopped
+
+            err = app_stop(); // Stop the app controller
+            if(err != APP_ERR_OK) {
+                log_error("app_stop failed (err: %d)", err);
+                return EXIT_FAILURE;
+            }
+
+            err = app_deinit(); // Deinit the controller
+            if(err != APP_ERR_OK) {
+                log_error("app_deinit failed (err: %d)", err);
+                return EXIT_FAILURE;
+            }
+
+            return EXIT_SUCCESS; // Exit
+        }
     }
-    return 0;
+
+    // This should never be reached
+    return EXIT_SUCCESS;
 }
